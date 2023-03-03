0cc382ce430ae393ed8cb37cc84c9ace973d8f70
Damien-Collot <dcollot76@gmail.com>
2023-03-03 21:19:12 +0100

    Feat : Diagramme de classe

--- Tp1_Automate/src/Main.java
+++ Tp1_Automate/src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         var AUto = new Automate("Exemple.json");
         System.out.println(AUto.appartient("aababba"));
-
     }
 }
