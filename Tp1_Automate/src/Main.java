0de78933037506972415b2e1f6c248b57f5d1053
Nicolas Wrobel <nicolas.18.wrobel@gmail.com>
2023-03-17 16:07:25 +0100

    feat : gestion de l'automate du distributeur

--- Tp1_Automate/src/Main.java
+++ Tp1_Automate/src/Main.java
@@ -1,6 +1,24 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        var AUto = new Automate("Exemple.json");
-        System.out.println(AUto.appartient("aababba"));
+        var Auto = new Automate("C:\\Users\\nicol\\Documents\\Cours\\Automate et langage\\TP\\Tp1_Automate\\Distributeur.json");
+        try {
+            File file = new File("C:\\Users\\nicol\\Documents\\Cours\\Automate et langage\\TP\\Tp1_Automate\\testDistributeur.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                System.out.println("Test du mot : " + line + " | " + Auto.appartient(line));
+                System.out.println("----------------");
+            }
+            fr.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
